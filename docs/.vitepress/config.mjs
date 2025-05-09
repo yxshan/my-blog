5258fb5fa86f20bfd430e309734a2036275f7f47
江林 <1851114089@qq.com>
2025-05-09 13:50:13 +0800

    init

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Blog",
+  description: "A personal blog built with VitePress",
+  base: "/",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Posts", link: "/posts/" },
+    ],
+
+    sidebar: [
+      {
+        text: "Posts",
+        items: [{ text: "First Post", link: "/posts/first-post" }],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
